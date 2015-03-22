eddeca9e6aede0cbc41e2e1068c775418ab8b255
ASzot <SzotAndrew@live.com>
2015-03-21 20:32:02 -0700

    Removed all of the physics stuff for now

--- MathSolverWebsite/Site.Master.designer.cs
+++ MathSolverWebsite/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace MathSolverWebsite {
         protected global::System.Web.UI.HtmlControls.HtmlAnchor evalLink;
         
         /// <summary>
-        /// equationsLink control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor equationsLink;
-        
-        /// <summary>
         /// helpLink control.
         /// </summary>
         /// <remarks>
